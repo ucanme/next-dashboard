f7e538c4f75af9c280957c1ea5abb3437a70671a
Ucan <admin@ucan.ME>
2023-11-26 16:14:21 +0800

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import { lusitana } from '@/app/ui/fonts/fonts'
+import { lusitana } from '@/app/ui/fonts'
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
